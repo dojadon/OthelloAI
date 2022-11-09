53ca7be90096260bbd1250ccdf6397a1999ecc3b
dojadon <zyando.iron79@gmail.com>
2022-11-10 01:17:43 +0900

    2022/11/10

---
+++ Trainer.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace OthelloAI
+{
+    public class TrainingData : List<TrainingDataElement>
+    {
+        public TrainingData()
+        {
+        }
+
+        public TrainingData(List<TrainingDataElement> data)
+        {
+            AddRange(data);
+        }
+
+        public void Add(Board b, int result)
+        {
+            Add(new TrainingDataElement(b, result));
+        }
+
+        public void Add(IEnumerable<Board> boards, float result)
+        {
+            AddRange(boards.Select(b => new TrainingDataElement(b, result)));
+        }
+    }
+
+    public struct TrainingDataElement
+    {
+        public readonly Board board;
+        public readonly float result;
+
+        public TrainingDataElement(Board board, float result)
+        {
+            this.board = board;
+            this.result = result;
+        }
+    }
+
+    public class TrainerUtil
+    {
+        public static TrainingData PlayForTrainingParallel(int n_game, Player player)
+        {
+            return PlayForTrainingParallel(n_game, player, false);
+        }
+
+        public static TrainingData PlayForTrainingParallel(int n_game, Player player, bool bi_result)
+        {
+            static bool Step(ref Board board, List<Board> boards, Player player, int stone)
+            {
+                (int x, int y, ulong move) = player.DecideMove(board, stone);
+                if (move != 0)
+                {
+                    board = board.Reversed(move, stone);
+                    boards.Add(board);
+                    return true;
+                }
+                return false;
+            }
+
+            int GetResult(Board b)
+            {
+                int r = b.GetStoneCountGap();
+                if (!bi_result)
+                    return r;
+
+                return Math.Max(-1, Math.Min(1, r));
+            }
+
+            int count = 0;
+
+            var data = Enumerable.Range(0, 16).AsParallel().Select(i =>
+            {
+                var results = new TrainingData();
+                var rand = new Random();
+
+                while (count < n_game)
+                {
+                    Board board = Tester.CreateRandomGame(8, rand);
+                    List<Board> boards = new List<Board>();
+
+                    while (Step(ref board, boards, player, 1) | Step(ref board, boards, player, -1))
+                    {
+                    }
+                    results.Add(boards, GetResult(board));
+
+                    Interlocked.Increment(ref count);
+                }
+
+                return results;
+            }).ToList().SelectMany(d => d);
+
+            return new TrainingData(data.ToList());
+        }
+
+        public static TrainingData PlayForTraining(int n_game, Player player, Random rand, bool bi_result)
+        {
+            static bool Step(ref Board board, List<Board> boards, Player player, int stone)
+            {
+                (_, _, ulong move) = player.DecideMove(board, stone);
+                if (move != 0)
+                {
+                    board = board.Reversed(move, stone);
+                    boards.Add(board);
+                    return true;
+                }
+                return false;
+            }
+
+            int GetResult(Board b)
+            {
+                int r = b.GetStoneCountGap();
+                if (!bi_result)
+                    return r;
+
+                return Math.Max(-1, Math.Min(1, r));
+            }
+
+            var results = new TrainingData();
+
+            for (int i = 0; i < n_game; i++)
+            {
+                Board board = Tester.CreateRandomGame(8, rand);
+                List<Board> boards = new List<Board>();
+
+                while (Step(ref board, boards, player, 1) | Step(ref board, boards, player, -1))
+                {
+                }
+                results.Add(boards, GetResult(board));
+            }
+
+            return results;
+        }
+    }
+
+    public class Trainer
+    {
+        public Weights Weights { get; }
+        public float LearningRate { get; }
+
+        public List<float> Log { get; } = new List<float>();
+
+        public Trainer(Weights weights, float lr)
+        {
+            Weights = weights;
+            LearningRate = lr;
+        }
+
+        public float Update(Board board, float result)
+        {
+            var boards = new RotatedAndMirroredBoards(board);
+
+            float e = result - Weights.EvalTraining(boards);
+
+            foreach (var b in boards)
+            {
+                Weights.UpdataEvaluation(b, e * LearningRate, 6);
+            }
+
+            Log.Add(e * e);
+
+            return e;
+        }
+
+        public static void Train(Weights weights, float lr, RecordReader reader)
+        {
+            var trainer = new Trainer(weights, lr);
+            reader.OnLoadMove += (b, r) => trainer.Update(b, r);
+            reader.OnLoadGame += i =>
+            {
+                if (i % 50000 == 0)
+                    Console.WriteLine(i);
+            };
+            reader.Read();
+        }
+    }
+}
---
+++ Weights.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace OthelloAI
+{
+    public abstract class Weights
+    {
+        public const float WEIGHT_RANGE = 10;
+
+        public static Weights Create(BoardHasher hasher, int n_stages, string file = "", bool load = false)
+        {
+            Weights[] weights_stage = Enumerable.Range(0, n_stages).Select(_ => new WeightsArray(hasher)).ToArray();
+            var weights = new WeightsStagebased(weights_stage)
+            {
+                FilePath = file,
+            };
+
+            if (load)
+                weights.Load();
+
+            return weights;
+        }
+
+        public string FilePath { get; set; }
+
+        public abstract void Reset();
+        public abstract int Eval(RotatedAndMirroredBoards b);
+        public abstract float EvalTraining(RotatedAndMirroredBoards b);
+        public abstract int NumOfEvaluation(int n_discs);
+
+        public abstract void UpdataEvaluation(Board board, float add, float range);
+        public abstract void ApplyTrainedEvaluation(float range);
+
+        public abstract float[] GetWeights();
+
+        public abstract void Read(BinaryReader reader);
+        public abstract void Write(BinaryWriter writer);
+
+        public static byte ConvertToInt8(float x, float range)
+        {
+            return (byte)Math.Clamp(x / range * 127 + 128, 0, 255);
+        }
+
+        public void Load()
+        {
+            using var reader = new BinaryReader(new FileStream(FilePath, FileMode.Open));
+            Read(reader);
+        }
+
+        public void Save()
+        {
+            using var writer = new BinaryWriter(new FileStream(FilePath, FileMode.Create));
+            Write(writer);
+        }
+    }
+
+    public class WeightsSum : Weights
+    {
+        Weights[] Weights { get; }
+
+        public WeightsSum(params Weights[] weights)
+        {
+            Weights = weights;
+        }
+
+        public override float[] GetWeights()
+        {
+            return Weights.SelectMany(w => w.GetWeights()).ToArray();
+        }
+
+        public override void Reset()
+        {
+            foreach (var w in Weights)
+                w.Reset();
+        }
+
+        public override void UpdataEvaluation(Board board, float add, float range)
+        {
+            foreach (var w in Weights)
+                w.UpdataEvaluation(board, add, range);
+        }
+
+        public override int Eval(RotatedAndMirroredBoards b)
+        {
+            return Weights.Sum(w => w.Eval(b));
+        }
+
+        public override float EvalTraining(RotatedAndMirroredBoards b)
+        {
+            return Weights.Sum(w => w.EvalTraining(b));
+        }
+
+        public override int NumOfEvaluation(int n_discs)
+        {
+            return Weights.Sum(w => w.NumOfEvaluation(n_discs));
+        }
+
+        public override void ApplyTrainedEvaluation(float range)
+        {
+            foreach (var w in Weights)
+                w.ApplyTrainedEvaluation(range);
+        }
+
+        public override void Read(BinaryReader reader)
+        {
+            foreach (var w in Weights)
+                w.Read(reader);
+        }
+
+        public override void Write(BinaryWriter writer)
+        {
+            foreach (var w in Weights)
+                w.Write(writer);
+        }
+    }
+
+    public class WeightsStagebased : Weights
+    {
+        Weights[] Weights { get; }
+
+        public WeightsStagebased(Weights[] weights)
+        {
+            Weights = weights;
+        }
+
+        public static int GetStage(int n_stone, int n_div)
+        {
+            return (n_stone - 5) / (60 / n_div);
+        }
+
+        protected Weights GetCurrentWeights(Board board)
+        {
+            return GetCurrentWeights(board.n_stone);
+        }
+
+        protected Weights GetCurrentWeights(int n_discs)
+        {
+            return Weights[GetStage(n_discs, Weights.Length)];
+        }
+
+        public override float[] GetWeights()
+        {
+            return Weights.SelectMany(w => w.GetWeights()).ToArray();
+        }
+
+        public override void Reset()
+        {
+            foreach (var w in Weights)
+                w.Reset();
+        }
+
+        public override void UpdataEvaluation(Board board, float add, float range)
+        {
+            GetCurrentWeights(board).UpdataEvaluation(board, add, range);
+        }
+
+        public override int Eval(RotatedAndMirroredBoards b)
+        {
+            return GetCurrentWeights(b.rot0).Eval(b);
+        }
+
+        public override float EvalTraining(RotatedAndMirroredBoards b)
+        {
+            return GetCurrentWeights(b.rot0).EvalTraining(b);
+        }
+
+        public override int NumOfEvaluation(int n_discs)
+        {
+            return GetCurrentWeights(n_discs).NumOfEvaluation(n_discs);
+        }
+
+        public override void ApplyTrainedEvaluation(float range)
+        {
+            foreach (var w in Weights)
+                w.ApplyTrainedEvaluation(range);
+        }
+
+        public override void Read(BinaryReader reader)
+        {
+            foreach (var w in Weights)
+                w.Read(reader);
+        }
+
+        public override void Write(BinaryWriter writer)
+        {
+            foreach (var w in Weights)
+                w.Write(writer);
+        }
+    }
+
+    public class WeightsArray : Weights
+    {
+        public BoardHasher Hasher { get; }
+        public SymmetricType Type { get; set; }
+
+        public float[] Weights { get; private set; }
+        protected byte[] WeightsB { get; private set; }
+
+        public override float[] GetWeights() => Weights;
+
+        public int NumOfStates { get; }
+
+        public WeightsArray(BoardHasher hasher)
+        {
+            Hasher = hasher;
+            NumOfStates = Hasher.NumOfStates;
+
+            Type = Hasher.SymmetricType;
+
+            Reset();
+        }
+
+        public override void Reset()
+        {
+            Weights = new float[Hasher.ArrayLength];
+            WeightsB = new byte[Hasher.ArrayLength];
+        }
+
+        public int Eval(Board b)
+        {
+            return WeightsB[Hasher.Hash(b)];
+        }
+
+        public override int Eval(RotatedAndMirroredBoards b)
+        {
+            return Type switch
+            {
+                SymmetricType.X_SYMMETRIC => Eval(b.rot0) + Eval(b.inv_rot0) + Eval(b.inv_rot90) + Eval(b.rot270) - 128 * 4,
+                SymmetricType.XY_SYMMETRIC => Eval(b.rot0) + Eval(b.inv_rot0) + Eval(b.inv_rot90) + Eval(b.rot90) - 128 * 4,
+                SymmetricType.DIAGONAL => Eval(b.rot0) + Eval(b.inv_rot0) - 128 * 2,
+                SymmetricType.ASYMMETRIC => Eval(b.rot0) + Eval(b.inv_rot0) + Eval(b.rot90) + Eval(b.inv_rot90) +
+                                                            Eval(b.rot180) + Eval(b.inv_rot180) + Eval(b.rot270) + Eval(b.inv_rot270) - 128 * 8,
+                _ => throw new NotImplementedException(),
+            };
+        }
+
+        public float EvalTraining(Board b)
+        {
+            return Weights[Hasher.Hash(b)];
+        }
+
+        public override float EvalTraining(RotatedAndMirroredBoards b)
+        {
+            return Type switch
+            {
+                SymmetricType.X_SYMMETRIC => EvalTraining(b.rot0) + EvalTraining(b.inv_rot0) + EvalTraining(b.inv_rot90) + EvalTraining(b.rot270),
+                SymmetricType.XY_SYMMETRIC => EvalTraining(b.rot0) + EvalTraining(b.inv_rot0) + EvalTraining(b.inv_rot90) + EvalTraining(b.rot90),
+                SymmetricType.DIAGONAL => EvalTraining(b.rot0) + EvalTraining(b.inv_rot0),
+                SymmetricType.ASYMMETRIC => EvalTraining(b.rot0) + EvalTraining(b.inv_rot0) + EvalTraining(b.rot90) + EvalTraining(b.inv_rot90) +
+                                                            EvalTraining(b.rot180) + EvalTraining(b.inv_rot180) + EvalTraining(b.rot270) + EvalTraining(b.inv_rot270),
+                _ => throw new NotImplementedException(),
+            };
+        }
+
+        public override int NumOfEvaluation(int n_discs) => Type switch
+        {
+            SymmetricType.X_SYMMETRIC => 4,
+            SymmetricType.XY_SYMMETRIC => 4,
+            SymmetricType.DIAGONAL => 2,
+            SymmetricType.ASYMMETRIC => 8,
+            _ => throw new NotImplementedException(),
+        };
+
+        public override void UpdataEvaluation(Board board, float add, float range)
+        {
+            int hash = Hasher.Hash(board);
+            int flipped = Hasher.FlipHash(hash);
+
+            Weights[hash] += add;
+            Weights[flipped] -= add;
+
+            WeightsB[hash] = ConvertToInt8(Weights[hash], range);
+            WeightsB[flipped] = ConvertToInt8(Weights[flipped], range);
+        }
+
+        public override void ApplyTrainedEvaluation(float range)
+        {
+            for (int i = 0; i < NumOfStates; i++)
+            {
+                uint index = Hasher.ConvertStateToHash(i);
+                WeightsB[index] = ConvertToInt8(Weights[index], range);
+            }
+        }
+
+        public override void Read(BinaryReader reader)
+        {
+            for (int i = 0; i < NumOfStates; i++)
+            {
+                float e = reader.ReadSingle();
+
+                uint index = Hasher.ConvertStateToHash(i);
+                Weights[index] = e;
+                WeightsB[index] = ConvertToInt8(Weights[index], WEIGHT_RANGE);
+            }
+        }
+
+        public override void Write(BinaryWriter writer)
+        {
+            for (int i = 0; i < NumOfStates; i++)
+            {
+                uint index = Hasher.ConvertStateToHash(i);
+
+                float e = Weights[index];
+                writer.Write(e);
+            }
+        }
+    }
+}
