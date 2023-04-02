a7b5290d38d3dcd7fa4aac0c86642d56efe2d75d
dojadon <zyando.iron79@gmail.com>
2023-04-03 07:00:02 +0900

    2023/04/03

--- Condingame/EdaxWeightLight.cs
+++ Condingame/EdaxWeightLight.cs
@@ -10,13 +10,13 @@ namespace OthelloAI.Condingame
     public class EdaxWeightLight
     {
         const int A1 = 0, B1 = 1, C1 = 2, D1 = 3, E1 = 4, F1 = 5, G1 = 6, H1 = 7;
-        const int A2 = 8 + 0, B2 = 8 + 1, C2 = 8 + 2, D2 = 8 + 3, E2 = 8 + 4, F2 = 8 + 5, G2 = 8 + 6, H2 = 8 + 7;
-        const int A3 = 16 + 0, B3 = 16 + 1, C3 = 16 + 2, D3 = 16 + 3, E3 = 16 + 4, F3 = 16 + 5, G3 = 16 + 6, H3 = 16 + 7;
-        const int A4 = 24 + 0, B4 = 24 + 1, C4 = 24 + 2, D4 = 24 + 3, E4 = 24 + 4, F4 = 24 + 5, G4 = 24 + 6, H4 = 24 + 7;
-        const int A5 = 32 + 0, B5 = 32 + 1, C5 = 32 + 2, D5 = 32 + 3, E5 = 32 + 4, F5 = 32 + 5, G5 = 32 + 6, H5 = 32 + 7;
-        const int A6 = 40 + 0, B6 = 40 + 1, C6 = 40 + 2, D6 = 40 + 3, E6 = 40 + 4, F6 = 40 + 5, G6 = 40 + 6, H6 = 40 + 7;
-        const int A7 = 48 + 0, B7 = 48 + 1, C7 = 48 + 2, D7 = 48 + 3, E7 = 48 + 4, F7 = 48 + 5, G7 = 48 + 6, H7 = 48 + 7;
-        const int A8 = 56 + 0, B8 = 56 + 1, C8 = 56 + 2, D8 = 56 + 3, E8 = 56 + 4, F8 = 56 + 5, G8 = 56 + 6, H8 = 56 + 7;
+        const int A2 = 8, B2 = 9, C2 = 10, D2 = 11, E2 = 12, F2 = 13, G2 = 14, H2 = 15;
+        const int A3 = 16, B3 = 17, C3 = 18, D3 = 19, E3 = 20, F3 = 21, G3 = 22, H3 = 23;
+        const int A4 = 24, B4 = 25, C4 = 26, D4 = 27, E4 = 28, F4 = 29, G4 = 30, H4 = 31;
+        const int A5 = 32, B5 = 33, C5 = 34, D5 = 35, E5 = 36, F5 = 37, G5 = 38, H5 = 39;
+        const int A6 = 40, B6 = 41, C6 = 42, D6 = 43, E6 = 44, F6 = 45, G6 = 46, H6 = 47;
+        const int A7 = 48, B7 = 49, C7 = 50, D7 = 51, E7 = 52, F7 = 53, G7 = 54, H7 = 55;
+        const int A8 = 56, B8 = 57, C8 = 58, D8 = 59, E8 = 60, F8 = 61, G8 = 62, H8 = 63;
         const int PASS = 64, NOMOVE = 65;
 
         public static int[][] EVAL_F2X = {
@@ -97,17 +97,17 @@ namespace OthelloAI.Condingame
     226314,
 };
 
-        static int[][] EVAL_C10 = { new int[59049], new int[59049] };
-        static int[][] EVAL_S10 = { new int[59049], new int[59049] };
-        static int[][] EVAL_C9 = { new int[19683], new int[19683] };
-        static int[][] EVAL_S8 = { new int[6561], new int[6561] };
-        static int[][] EVAL_S7 = { new int[2187], new int[2187] };
-        static int[][] EVAL_S6 = { new int[729], new int[729] };
-        static int[][] EVAL_S5 = { new int[243], new int[243] };
-        static int[][] EVAL_S4 = { new int[81], new int[81] };
+        static int[] EVAL_C10 =  new int[59049];
+        static int[] EVAL_S10 =  new int[59049];
+        static int[] EVAL_C9 = new int[19683];
+        static int[] EVAL_S8 =  new int[6561];
+        static int[] EVAL_S7 =  new int[2187];
+        static int[] EVAL_S6 =  new int[729];
+        static int[] EVAL_S5 =  new int[243];
+        static int[] EVAL_S4 =  new int[81] ;
 
-        readonly static int[] EVAL_PACKED_SIZE = { 10206, 29889, 29646, 29646, 3321, 3321, 3321, 3321, 1134, 378, 135, 45, 1 };
-        readonly static int[] EVAL_SIZE = { 19683, 59049, 59049, 59049, 6561, 6561, 6561, 6561, 2187, 729, 243, 81, 1 };
+        readonly static int[] PS = { 10206, 29889, 29646, 29646, 3321, 3321, 3321, 3321, 1134, 378, 135, 45, 1 };
+        readonly static int[] S = { 19683, 59049, 59049, 59049, 6561, 6561, 6561, 6561, 2187, 729, 243, 81, 1 };
 
         /** number of (unpacked) weights */
         const int EVAL_N_WEIGHT = 226315;
@@ -118,14 +118,9 @@ namespace OthelloAI.Condingame
 
         static int[] o = { 1, 0, 2 };
 
-        static int opponent_feature(int l, int d)
-        {
-            int f = o[l % 3];
-            if (d > 1) f += opponent_feature(l / 3, d - 1) * 3;
-            return f;
-        }
+        static byte[][] weight;
 
-        public static byte[][] Open(BinaryReader reader)
+        public static byte[][] Open(byte[] w)
         {
             int[] T = new int[59049];
             int i, j, k, l, n;
@@ -138,8 +133,7 @@ namespace OthelloAI.Condingame
                 ((l / 3) % 3) * 729 + (l % 3) * 2187;
                 if (k < l) T[l] = T[k];
                 else T[l] = n++;
-                EVAL_S8[0][l] = T[l];
-                EVAL_S8[1][opponent_feature(l, 8)] = T[l];
+                EVAL_S8[l] = T[l];
             }
             for (l = n = 0; l < 2187; l++)
             { /* 7 squares : 2187 -> 1134 */
@@ -148,7 +142,7 @@ namespace OthelloAI.Condingame
                  (l % 3) * 729;
                 if (k < l) T[l] = T[k];
                 else T[l] = n++;
-                EVAL_S7[0][l] = T[l];
+                EVAL_S7[l] = T[l];
             }
             for (l = n = 0; l < 729; l++)
             { /* 6 squares : 729 -> 378 */
@@ -156,7 +150,7 @@ namespace OthelloAI.Condingame
                  ((l / 9) % 3) * 27 + ((l / 3) % 3) * 81 + (l % 3) * 243;
                 if (k < l) T[l] = T[k];
                 else T[l] = n++;
-                EVAL_S6[0][l] = T[l];
+                EVAL_S6[l] = T[l];
             }
             for (l = n = 0; l < 243; l++)
             { /* 5 squares : 243 -> 135 */
@@ -164,14 +158,14 @@ namespace OthelloAI.Condingame
                 ((l / 3) % 3) * 27 + (l % 3) * 81;
                 if (k < l) T[l] = T[k];
                 else T[l] = n++;
-                EVAL_S5[0][l] = T[l];
+                EVAL_S5[l] = T[l];
             }
             for (l = n = 0; l < 81; l++)
             { /* 4 squares : 81 -> 45 */
                 k = ((l / 27) % 3) + ((l / 9) % 3) * 3 + ((l / 3) % 3) * 9 + (l % 3) * 27;
                 if (k < l) T[l] = T[k];
                 else T[l] = n++;
-                EVAL_S4[0][l] = T[l];
+                EVAL_S4[l] = T[l];
             }
             for (l = n = 0; l < 19683; l++)
             { /* 9 corner squares : 19683 -> 10206 */
@@ -180,7 +174,7 @@ namespace OthelloAI.Condingame
                 ((l / 81) % 3) * 27 + ((l / 3) % 3) * 9 + ((l / 9) % 3) * 3 + (l % 3);
                 if (k < l) T[l] = T[k];
                 else T[l] = n++;
-                EVAL_C9[0][l] = T[l];
+                EVAL_C9[l] = T[l];
             }
             for (l = n = 0; l < 59049; l++)
             { /* 10 squares (edge +X ) : 59049 -> 29646 */
@@ -190,7 +184,7 @@ namespace OthelloAI.Condingame
                   (l % 3) * 19683;
                 if (k < l) T[l] = T[k];
                 else T[l] = n++;
-                EVAL_S10[0][l] = T[l];
+                EVAL_S10[l] = T[l];
             }
             for (l = n = 0; l < 59049; l++)
             { /* 10 squares (angle + X) : 59049 -> 29889 */
@@ -200,96 +194,91 @@ namespace OthelloAI.Condingame
                   (l % 3) * 19683;
                 if (k < l) T[l] = T[k];
                 else T[l] = n++;
-                EVAL_C10[0][l] = T[l];
+                EVAL_C10[l] = T[l];
             }
 
-            int n_w = 114364;
-            byte[][] EVAL_WEIGHT = EVAL_N_PLY.Loop(_ => new byte[EVAL_N_WEIGHT]).ToArray();
-
-            byte[] header = reader.ReadBytes(28);
-            // Console.WriteLine(string.Join(", ", header));
+            weight = EVAL_N_PLY.Loop(_ => new byte[EVAL_N_WEIGHT]).ToArray();
 
             for (int ply = 0; ply < EVAL_N_PLY; ply++)
             {
-                byte[] w = reader.ReadBytes(n_w);
-
-                i = j = offset = 0;
-                for (k = 0; k < EVAL_SIZE[i]; k++, j++)
+                offset = 114364 * ply;
+                i = j = 0;
+                for (k = 0; k < S[i]; k++, j++)
                 {
-                    EVAL_WEIGHT[ply][j] = w[EVAL_C9[0][k] + offset];
+                    weight[ply][j] = w[EVAL_C9[k] + offset];
                 }
-                offset += EVAL_PACKED_SIZE[i];
+                offset += PS[i];
                 i++;
-                for (k = 0; k < EVAL_SIZE[i]; k++, j++)
+                for (k = 0; k < S[i]; k++, j++)
                 {
-                    EVAL_WEIGHT[ply][j] = w[EVAL_C10[0][k] + offset];
+                    weight[ply][j] = w[EVAL_C10[k] + offset];
                 }
-                offset += EVAL_PACKED_SIZE[i];
+                offset += PS[i];
                 i++;
-                for (k = 0; k < EVAL_SIZE[i]; k++, j++)
+                for (k = 0; k < S[i]; k++, j++)
                 {
-                    EVAL_WEIGHT[ply][j] = w[EVAL_S10[0][k] + offset];
+                    weight[ply][j] = w[EVAL_S10[k] + offset];
                 }
-                offset += EVAL_PACKED_SIZE[i];
+                offset += PS[i];
                 i++;
-                for (k = 0; k < EVAL_SIZE[i]; k++, j++)
+                for (k = 0; k < S[i]; k++, j++)
                 {
-                    EVAL_WEIGHT[ply][j] = w[EVAL_S10[0][k] + offset];
+                    weight[ply][j] = w[EVAL_S10[k] + offset];
                 }
-                offset += EVAL_PACKED_SIZE[i];
+                offset += PS[i];
                 i++;
-                for (k = 0; k < EVAL_SIZE[i]; k++, j++)
+                for (k = 0; k < S[i]; k++, j++)
                 {
-                    EVAL_WEIGHT[ply][j] = w[EVAL_S8[0][k] + offset];
+                    weight[ply][j] = w[EVAL_S8[k] + offset];
                 }
-                offset += EVAL_PACKED_SIZE[i];
+                offset += PS[i];
                 i++;
-                for (k = 0; k < EVAL_SIZE[i]; k++, j++)
+                for (k = 0; k < S[i]; k++, j++)
                 {
-                    EVAL_WEIGHT[ply][j] = w[EVAL_S8[0][k] + offset];
+                    weight[ply][j] = w[EVAL_S8[k] + offset];
                 }
-                offset += EVAL_PACKED_SIZE[i];
+                offset += PS[i];
                 i++;
-                for (k = 0; k < EVAL_SIZE[i]; k++, j++)
+                for (k = 0; k < S[i]; k++, j++)
                 {
-                    EVAL_WEIGHT[ply][j] = w[EVAL_S8[0][k] + offset];
+                    weight[ply][j] = w[EVAL_S8[k] + offset];
                 }
-                offset += EVAL_PACKED_SIZE[i];
+                offset += PS[i];
                 i++;
-                for (k = 0; k < EVAL_SIZE[i]; k++, j++)
+                for (k = 0; k < S[i]; k++, j++)
                 {
-                    EVAL_WEIGHT[ply][j] = w[EVAL_S8[0][k] + offset];
+                    weight[ply][j] = w[EVAL_S8[k] + offset];
                 }
-                offset += EVAL_PACKED_SIZE[i];
+                offset += PS[i];
                 i++;
-                for (k = 0; k < EVAL_SIZE[i]; k++, j++)
+                for (k = 0; k < S[i]; k++, j++)
                 {
-                    EVAL_WEIGHT[ply][j] = w[EVAL_S7[0][k] + offset];
+                    weight[ply][j] = w[EVAL_S7[k] + offset];
                 }
-                offset += EVAL_PACKED_SIZE[i];
+                offset += PS[i];
                 i++;
-                for (k = 0; k < EVAL_SIZE[i]; k++, j++)
+                for (k = 0; k < S[i]; k++, j++)
                 {
-                    EVAL_WEIGHT[ply][j] = w[EVAL_S6[0][k] + offset];
+                    weight[ply][j] = w[EVAL_S6[k] + offset];
                 }
-                offset += EVAL_PACKED_SIZE[i];
+                offset += PS[i];
                 i++;
-                for (k = 0; k < EVAL_SIZE[i]; k++, j++)
+                for (k = 0; k < S[i]; k++, j++)
                 {
-                    EVAL_WEIGHT[ply][j] = w[EVAL_S5[0][k] + offset];
+                    weight[ply][j] = w[EVAL_S5[k] + offset];
                 }
-                offset += EVAL_PACKED_SIZE[i];
+                offset += PS[i];
                 i++;
-                for (k = 0; k < EVAL_SIZE[i]; k++, j++)
+                for (k = 0; k < S[i]; k++, j++)
                 {
-                    EVAL_WEIGHT[ply][j] = w[EVAL_S4[0][k] + offset];
+                    weight[ply][j] = w[EVAL_S4[k] + offset];
                 }
-                offset += EVAL_PACKED_SIZE[i];
+                offset += PS[i];
                 i++;
-                EVAL_WEIGHT[ply][j] = w[offset];
+                weight[ply][j] = w[offset];
             }
 
-            return EVAL_WEIGHT;
+            return weight;
         }
 
         static int board_get_square_color(B board, int x)
@@ -315,58 +304,35 @@ namespace OthelloAI.Condingame
             return feature;
         }
 
-        public static ulong ConvertToMask(int id)
+        const int SCORE_MIN = -64;
+        const int SCORE_MAX = 64;
+
+        public static int Eval(B b)
         {
-            int[] f2x = EVAL_F2X[id];
+            byte[] w = weight[b.n_stone - 4];
+            int[] f = CreateFeature(b);
 
-            ulong dst = 0;
-            for (int i = 0; i < f2x.Length; i++)
-            {
-                dst |= 1UL << f2x[i];
-            }
-            return dst;
-        }
+            int score = w[f[0]] + w[f[1]] + w[f[2]] + w[f[3]]
+                            + w[f[4]] + w[f[5]] + w[f[6]] + w[f[7]]
+                            + w[f[8]] + w[f[9]] + w[f[10]] + w[f[11]]
+                            + w[f[12]] + w[f[13]] + w[f[14]] + w[f[15]]
+                            + w[f[16]] + w[f[17]] + w[f[18]] + w[f[19]]
+                            + w[f[20]] + w[f[21]] + w[f[22]] + w[f[23]]
+                            + w[f[24]] + w[f[25]]
+                            + w[f[26]] + w[f[27]] + w[f[28]] + w[f[29]]
+                            + w[f[30]] + w[f[31]] + w[f[32]] + w[f[33]]
+                            + w[f[34]] + w[f[35]] + w[f[36]] + w[f[37]]
+                            + w[f[38]] + w[f[39]] + w[f[40]] + w[f[41]]
+                            + w[f[42]] + w[f[43]] + w[f[44]] + w[f[45]]
+                            + w[226314];
 
-        public static float[] ConvertToMyWeight(short[] w, int id)
-        {
-            int[] f2x = EVAL_F2X[id];
-            int offset = EVAL_OFFSET[id];
+            if (score > 0) score += 64; else score -= 64;
+            score /= 128;
 
-            int len = (int)Math.Pow(3, f2x.Length);
+            if (score <= SCORE_MIN) score = SCORE_MIN + 1;
+            else if (score >= SCORE_MAX) score = SCORE_MAX - 1;
 
-            float[] dst = new float[len];
-
-            int[] indices = f2x.OrderBy(x => x).Select(x => Array.IndexOf(f2x, x)).ToArray();
-            int[] color = { 1, 2, 0 };
-
-            for (int i = 0; i < len; i++)
-            {
-                int[] a = WeightUtil.Disassemble(i, f2x.Length);
-                a = a.Reverse().ToArray();
-                a = indices.Select(x => a[x]).Select(x => color[x]).ToArray();
-                int hash = WeightUtil.Assemble(a);
-
-                dst[hash] = w[i + offset] / 128F;
-            }
-
-            return dst;
-        }
-
-        public static float[] ConvertToMyWeight2(short[] w, int id)
-        {
-            int[] f2x = EVAL_F2X[id];
-            int offset = EVAL_OFFSET[id];
-
-            int len = (int)Math.Pow(3, f2x.Length);
-
-            float[] dst = new float[len];
-
-            for (int i = 0; i < len; i++)
-            {
-                dst[i] = w[i + offset] / 128F;
-            }
-
-            return dst;
+            return score;
         }
     }
 }
